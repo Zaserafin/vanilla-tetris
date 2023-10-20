75be00f25a40e116d5d39e86406c468a6102fe12
Zaserafin <mauro.zaserafin@gmail.com>
2023-10-20 08:44:59 -0300

    Added start button

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import "./stylesheets/style.css";
-
 import Tetris from "./tetris";
+
+const startButton = document.getElementById("start-button");
 
 const tetris = new Tetris();
 
-window.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowLeft") tetris.handleLeftMovement();
-  if (event.key === "ArrowRight") tetris.handleRightMovement();
-  if (event.key === "ArrowUp") tetris.handleRotationMovement();
+startButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  startButton.style.display = "none";
+  tetris.start();
 });
 
-tetris.start();
+window.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") tetris?.handleLeftMovement();
+  if (event.key === "ArrowRight") tetris?.handleRightMovement();
+  if (event.key === "ArrowUp") tetris?.handleRotationMovement();
+});
